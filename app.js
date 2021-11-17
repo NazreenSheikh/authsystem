db604fee43077175c3c6a411c9de1cec2d6b256c
nazreen <nazreenshaikh0077@gmail.com>
2021-11-17 10:22:35 +0530

    initial setup

---
+++ app.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express');
+// const User = require('./model/User')
+const app = express();
+
+app.use(express.json())
+
+app.get('/', (req, res) =>
+    res.send("<h3> hello from auth system </h3>")
+)
+
+app.post('/auth', async (req, res) => {
+
+    const { firstName, lastName, email, password } = req.body;
+    if (!email && !password && !firstName && !lastName) {
+        res.send("all feilds are required");
+    }
+    const existingEmail = await User.findOne({ email: email });
+
+    if (existingEmail) {
+        res.send("email already exists");
+    }
+}
+)
+
+
+module.exports = app;
