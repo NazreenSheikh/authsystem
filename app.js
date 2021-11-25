2c52f6422164907c7ad5676d73505c04adbd667e
nazreen <nazreenshaikh0077@gmail.com>
2021-11-26 00:00:07 +0530

    new route

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
 require('dotenv').config()
-const express = require('express');
+const express = require('express')
 // const User = require('./model/User')
-const app = express();
+const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) =>
-    res.send("<h3> hello from auth system </h3>")
-)
+app.get('/', (req, res) => res.send('<h3> hello from auth system </h3>'))
 
 app.post('/auth', async (req, res) => {
+  const { firstName, lastName, email, password } = req.body
+  if (!email && !password && !firstName && !lastName) {
+    res.status(400).send('all feilds are required')
+  }
+  const existingEmail = await User.findOne({ email: email })
 
-    const { firstName, lastName, email, password } = req.body;
-    if (!email && !password && !firstName && !lastName) {
-        res.send("all feilds are required");
-    }
-    const existingEmail = await User.findOne({ email: email });
+  if (existingEmail) {
+    res.status(401).send('email already exists')
+  }
+})
 
-    if (existingEmail) {
-        res.send("email already exists");
-    }
-}
-)
-
-
-module.exports = app;
+module.exports = app
